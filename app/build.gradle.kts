e1eed7d8ae7f1e56895a1360ed663cf3c418e148
E. M. Ekanayake <exe0003@my.londonmet.ac.uk>
2025-05-22 14:52:14 +0530

    fixing api key leak

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("string", "GOOGLE_API_KEY", "\$googleApiKey")
     }
 
     buildTypes {
